917318ffc83831d56c998f8731771068f664ad19
Oran C <Oranbusiness@gmail.com>
2020-07-02 19:25:21 -0700

    [add] mvp, plus slider, and generate button

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # marble-notebook-pattern-generator
  p5 program the generates the black/white marble covers used in college classes
+```
+by oran collins
+github.com/wisehackermonkey
+oranbusiness@gmail.com
+20200702
+
+```
+
+# Trying to imulate this book cover pattern
+![](https://images-na.ssl-images-amazon.com/images/I/91dFyARfTZL._AC_SL1500_.jpg)
+# Results
+
+![](Screenshot_5.png)
+
+-----
+# Dev log
+# WORKS! v1
+![](Screenshot_5.png)
+# added slider and generate button
+![](Screenshot_1.png)
--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
+let step = 0.06
+let p_size = 10
+let stepSlider 
+
+let genButton 
 function setup() {
     createCanvas(400, 400);
-  }
+  noStroke();
+  frameRate(60)
+  stepSlider = createSlider(0,1000, 336,1)
+  stepSlider.position(width/2, height+10)
+
+  genButton = createButton("Generate")
+  genButton.position(width-50, height+10)
+  genButton.mousePressed(render_marble)
+  render_marble()
+}
+
+function draw() {
+    
+    print(step)
+    if(frameCount%3==0){
+    render_marble()
+    }
+}
+
+
+function render_marble(){
+    step = map(stepSlider.value(),0,1000,0,0.389)
+    for (var x = 0; x < width; x+=1) {
+        for (var y = 0; y < height; y+=1) {
+            var c = 255 * noise(step * x, step * y);
   
-  function draw() {
-    background(50);
-  }
+          if(c > 100){
+              c= 0
+          }else{
+              c=255
+          }
+            fill(c);
+            rect(x, y, p_size, p_size);
+        }		
+      }
+  
+}
